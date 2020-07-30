849c55628e9c9ba66b0727bf6c65c7bb3adb63ae
truong <truongptk30a3@gmail.com>
2020-07-30 22:01:04 +0900

    simple meo

--- src/meo.c
+++ src/meo.c
@@ -9,13 +9,33 @@
 #include "gen.h"
 #include "meo.h"
 
+typedef struct MeoHandle {
+    void* lex_prm;
+    void* parse_prm;
+} MeoHandle;
+
 int main(int argc, char*argv[])
 {
     if (argc != 2) {
         printf ("input parameter error\n");
         return -1;
     }
-    printf("Hello, World\n");
+
+    MeoHandle meo_handle = {NULL, NULL};
+
+    LexCreate();
+    LexOpen(&(meo_handle.lex_prm), argv[1]);
+
+    ParseCreate();
+    ParseOpen(&(meo_handle.parse_prm), meo_handle.lex_prm);
+
+    ParseProc(meo_handle.parse_prm);
+
+    ParseClose(meo_handle.parse_prm);
+    LexClose(meo_handle.lex_prm);
+
+    LexDestroy();
+    ParseDestroy();
 
     return 0;
 }
