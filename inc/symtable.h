ab388946f7a9677c68e027124709ec640bd639f7
Pham Trong Truong <truongptk30a3@gmail.com>
2020-08-12 20:52:20 +0900

    add template symtable

---
+++ inc/symtable.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020/08 gravieb <truongptk30a3@gmail.com>
+ * This file is released under the GPLv3
+ */
+
+#ifndef _SYM_TABLE_H_
+#define _SYM_TABLE_H_
+
+
+typedef struct SymbolTable {
+    char *name;
+} SymbolTable;
+
+
+#endif
---
+++ src/symtable.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020/08 gravieb <truongptk30a3@gmail.com>
+ * This file is released under the GPLv3
+ */
+
+/*
+ * Symtable for quick simple version. poor performance.
+ * It will be replace by hashtable version later.
+ */
+
+typedef struct SymbolTable {
+    char *name;
+} SymbolTable;
+
