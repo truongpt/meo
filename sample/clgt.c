b9288d96a7a0fcb2b03c90ebbb8689cdc8dc29f5
truong <truongptk30a3@gmail.com>
2020-08-26 22:23:27 +0900

    add sample clgt

---
+++ sample/clgt.c
@@ -0,0 +1,15 @@
+{
+    int i;
+    int j;
+    int k;
+    i = 1;
+    j = 2;
+    k = 3;
+    k = i + j * k;
+    int a;
+    a = i > j;
+    i = j == j;
+    print k;
+    print a;
+    print i;
+}
